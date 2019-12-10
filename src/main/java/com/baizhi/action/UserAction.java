f575088db957e12a487c42b6ff7de26003f1d13e
tlighc <18635791392@163.com>
2019-12-10 21:57:48 +0800

    anno项目

---
+++ src/main/java/com/baizhi/action/UserAction.java
@@ -0,0 +1,6 @@
+package com.baizhi.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+}
---
+++ src/main/test/java/TestFunction.java
@@ -0,0 +1,21 @@
+import com.baizhi.dao.UserDao;
+import com.baizhi.entity.User;
+import com.baizhi.service.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/applicationContext.xml")
+public class TestFunction {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserDao userDao;
+    @Test
+    public void Testfunction(){
+        userService.register(new User(null,"zhangda","123",89.5));
+    }
+}
